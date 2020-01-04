40fec2f99ed6bd1e94cacfc8cae9394a7443d299
Rasmus Andersson <rasmus@notion.se>
2020-01-04 10:35:15 -0800

    remove unused code

--- src/ns.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "sym.h"
-
-// Namespace maps names to nodes.
-// .bindings are filled in during parsing and accessed during evaluation (for looking up symbols).
-typedef struct Namespace {
-  Str    qname;    // Qualified name, i.e. "user.foo.bar.". Always ends in "."
-  SymMap bindings; // Unqualified names to expressions defined in this namespace.
-} Namespace;
